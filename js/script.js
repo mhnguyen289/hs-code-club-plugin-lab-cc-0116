270f396e3a6ee16f7a6814c58001cf656ae05a2a
mhnguyen289 <mhnguyen289@gmail.com>
2016-04-19 15:17:44 -0700

    Done.

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $('#funny').funnyText({
+    speed: 700,
+    borderColor: 'green',
+    activeColor: 'green',
+    color: 'black',
+    fontSize: '7em',
+    direction: 'both'
+  });
+});
